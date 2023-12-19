f587662a447f3586f7e4d07f9be853e2ac680c6b
Astr077 <je.martinez21@info.uas.edu.mx>
2023-12-19 13:59:37 -0700

    New branch

---
+++ README.md
@@ -0,0 +1,23 @@
+## POS con Laravel open source
+Pequeño punto de venta con Laravel 7(no lo pude hacer funcionar en laravel 8).
+[Bienvenida al sistema de ventas escrito en Laravel]
+## Características
+
+-   Utiliza Laravel en su última versión (a la fecha de escribir esto)
+-   Bootstrap es usado para los estilos
+-   Iconos de FontAwesome
+-   Sistema totalmente responsivo, es decir, funciona en móviles, tabletas y computadoras
+-   Control de inventario con precio de venta, precio de compra, utilidad, existencia, etcétera
+-   Opción para realizar venta, agregando productos
+-   Comprobación de existencia de inventario al vender
+-   Resta de existencia al vender
+-   Impresión de tickets de venta en impresora térmica
+-   Reporte de ventas
+-   Inicio de sesión y registro de usuarios
+-   App para Android
+
+## Créditos de las imágenes
+
+  
+
+**Productos:** Icons from [www.flaticon.com](https://www.flaticon.com/  "Flaticon")
---
+++ resources/views/misc/acerca_de.blade.php
@@ -0,0 +1,35 @@
+@extends("maestra")
+@section("titulo","Acerca de")
+@section("contenido")
+    <div class="row">
+        <div class="col-12">
+            <h1>Acerca de</h1>
+            <p>
+                Programado por <a href="">Jesus Eduardo Rodriguez Martinez</a>. Más 
+                <a href="https://api.whatsapp.com/send?phone=526674638730&text=Consulta%20a%20soporte%20tecnico%F0%9F%98%8A%F0%9F%91%8C">información</a>.
+                <br>
+                
+            </p>
+            <pre>
+                <code>
+ 
+                </code>
+            </pre>
+            <h2>Programadores</h2>
+            <ul>
+                <li>Jesus Eduardo Rodriguez Martinez</li>
+                <li>Roberto Carlos Valdez Leal</li>
+                <li>Jorge Guadalupe Paniagua Rivas</li>
+                <li>Jesus Noe Tapia Pérez</li>
+                <li>Jesús Enrique Cano Corvera</li>
+            </ul>
+
+            <h2>Créditos de las imágenes</h2>
+
+            <strong>Icons from&nbsp;</strong>
+            <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            <br>
+            
+        </div>
+    </div>
+@endsection
